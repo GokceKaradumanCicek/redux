8df1156a1cdf2cf242ecd7c42b14fc31c1246519
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-07 12:43:17 +0300

    Main draft of redux

--- redux-demo/redux-demo.js
+++ redux-demo/redux-demo.js
@@ -1,17 +1,21 @@
-const redux= require('redux');// importing third party pacakages
-const counterReducer=(state={ counter:0 },action)=>{
-    return {
-        counter: state.counter +1
-    };
+const redux= require('redux');// importing third party pacakages for redux
+
+const counterReducer=(state={ counter:0 },action)=>{ //reducer function that tahts state and action.State need to have initial value.
+    if(action.type=== 'increment'){
+        return {
+            counter: state.counter +1
+        };
+    }
+    return state;//for other functions returns unchanged state.
 }
-const store=redux.createStore(counterReducer) //redux is an object, creating store and receive reducer function
-console.log(store.getState());
+
+const store=redux.createStore(counterReducer) //redux is an object, creating store and store receives reducer function
 
 const counterSubscriber=()=>{ //subscriber function, no parameters
     const latestState=store.getState();// getState is avaliable method on created 'store',it gives latest state snapshot
     console.log(latestState) 
 }
  
-store.subscribe(counterSubscriber);
+store.subscribe(counterSubscriber);//defining counterSubscriber to store as a subscriber function.
 store.dispatch({type:'increment'});
-//action int he dispatch function is just a java script object with a type property which act as an identifier
+//action in the dispatch function is just a java script object with a type property which act as an identifier
