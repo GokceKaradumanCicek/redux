0da5c5042c707e35622532af0865b379454789c0
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-07 16:17:45 +0300

    using useSelector access to state of 'store' in component

--- firts-app-redux-react/src/components/Counter.js
+++ firts-app-redux-react/src/components/Counter.js
@@ -1,12 +1,22 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux'; //to access to 'store' we created.
 
 const Counter = () => {
+  const counter = useSelector(state=>state.counter)//this function will be executed for us by react redux,
+  //then it will pass redux state,
+  //get the part of state which this component is needed.This is 'counter' in this store.
+  //Now the great thing is that when you use useSelector,
+  //React Redux will automatically set up a subscription to the Redux store for this component.
+  //So your component will be updated and will receive the latest counter automatically
+  //whenever that data changes in the Redux store.
+  //So it's an automatically reactive and changes to the Redux store will cause this function to be executed.
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
