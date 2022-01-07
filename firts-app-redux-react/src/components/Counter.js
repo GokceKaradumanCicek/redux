3bb379903429c92eb362502c58aecf9793886e4f
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-07 16:57:50 +0300

    Import useDispatch() that returns a function,apply this function to Increment and Decrement buttons for click action with dispatching action type

--- firts-app-redux-react/src/components/Counter.js
+++ firts-app-redux-react/src/components/Counter.js
@@ -1,7 +1,8 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux'; //to access to 'store' we created.
+import { useSelector, useDispatch } from 'react-redux'; //to access to 'store' we created.
 
 const Counter = () => {
+  const dispatch=useDispatch(); //useDispatch returns a function
   const counter = useSelector(state=>state.counter)//this function will be executed for us by react redux,
   //then it will pass redux state,
   //get the part of state which this component is needed.This is 'counter' in this store.
@@ -11,12 +12,22 @@ const Counter = () => {
   //whenever that data changes in the Redux store.
   //So it's an automatically reactive and changes to the Redux store will cause this function to be executed.
 
+  const incrementHandler=()=>{
+    dispatch({type: 'increment'});
+  }
+  const decrementHandler=()=>{
+    dispatch({type: 'decrement'});
+  }
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button  onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
