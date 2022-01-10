35e1dad47528c1e6b34c3982dedf718fc4cc6c4e
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-10 17:42:41 +0300

    Dispatching new slice to multiple components(auth)

--- firts-app-redux-react/src/App.js
+++ firts-app-redux-react/src/App.js
@@ -2,12 +2,16 @@ import { Fragment } from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth=useSelector(state=>state.authReducer.isAuthenticated);//Getting authentication value
   return (
     <Fragment>
       <Header></Header>
-      <Auth />
+      {!isAuth && <Auth/>}
+      {isAuth && <UserProfile/>}
       <Counter />
     </Fragment>
   );
