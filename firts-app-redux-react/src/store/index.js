a160fa18420629b6823d953cef5cc2c83028e3ea
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-10 15:23:56 +0300

    using configureStore({reducer:...}) to create a store and passing reducer function of counterSlice

--- firts-app-redux-react/src/store/index.js
+++ firts-app-redux-react/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore } from "redux";
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, configureStore} from '@reduxjs/toolkit';
+import { configure } from "@testing-library/react";
 const initialState={ counter:0, showCounter:true }
 
-createSlice({ //This is used to mutate the state, and prevent writing whole state even if some properties are not used.
+const counterSlice=createSlice({ //This is used to mutate the state, and prevent writing whole state even if some properties are not used.
     name:'counterSlice', // name of slice(required)
     initialState: initialState, // initial state must be entered
     reducers:{
@@ -19,26 +20,30 @@ createSlice({ //This is used to mutate the state, and prevent writing whole stat
 })
 
 
-const counterReducer=(state=initialState,action)=>{
-    if(action.type==='increment'){
-        return{// returns overall state object so showCounter must be added,even if it is not used here.
-            counter:state.counter+ action.amount,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type==='decrement'){
-        return{
-            counter:state.counter-1,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type === 'toggle'){
-        return{
-            counter:state.counter,
-            showCounter: !state.showCounter //Must be the opposite of inital state 
-        }
-    }
-    return state;
-}
-const store=createStore(counterReducer);
+// const counterReducer=(state=initialState,action)=>{
+//     if(action.type==='increment'){
+//         return{// returns overall state object so showCounter must be added,even if it is not used here.
+//             counter:state.counter+ action.amount,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type==='decrement'){
+//         return{
+//             counter:state.counter-1,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type === 'toggle'){
+//         return{
+//             counter:state.counter,
+//             showCounter: !state.showCounter //Must be the opposite of inital state 
+//         }
+//     }
+//     return state;
+// }
+//const store=createStore(counterReducer);
+const store=configureStore({
+    reducer:counterSlice.reducer
+})//Creates a store,receive an object not a function.
+
 export default store;
