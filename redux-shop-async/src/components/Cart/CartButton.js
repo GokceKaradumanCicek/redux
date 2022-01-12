d5eae2c4a1a0600be125174226433184e5e44fdf
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-12 11:53:42 +0300

    Dispatch an action in CartButton and take state show Cart component according to state value

--- redux-shop-async/src/components/Cart/CartButton.js
+++ redux-shop-async/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
+import { uiAction } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
 
 const CartButton = (props) => {
+  const dispatch=useDispatch();
+  const toggleHandler=()=>{
+    dispatch(uiAction.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button onClick={toggleHandler} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
